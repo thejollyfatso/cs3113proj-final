b229a3ea762626ab1de21e8e9ab3765c46222b49
thejollyfatso <rafael.r.deleon@gmail.com>
2024-08-09 21:48:59 -0400

    parry fix

--- cs3113proj-final/Hitbox.cpp
+++ cs3113proj-final/Hitbox.cpp
@@ -138,7 +138,8 @@ void Hitbox::update(float delta_time, Hitbox* otherHitbox) {
         if (isColliding(otherHitbox) && m_active && !otherHitbox->m_active) {
             //this->m_hidden = false;
             //otherHitbox->m_hidden = false;
-            otherHitbox->m_entity->parry(m_entity->m_input_queue[0].stance, m_entity->m_input_queue[0].weight);
+            //otherHitbox->m_entity->parry(m_entity->m_input_queue[0].stance, m_entity->m_input_queue[0].weight);
+            otherHitbox->m_entity->parry(m_entity->m_atk_stance, m_entity->m_atk_weight);
         }
 		else { this->m_hidden = true; otherHitbox->m_hidden = true; } //  DEBUG
     }
